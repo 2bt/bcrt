adec7e34f4a64376f1b038048bbf56c53e941cf7
2bt <s8572327@gmail.com>
2020-04-07 15:40:13 +0200

    shorten

--- houndify.cpp
+++ houndify.cpp
@@ -13,8 +13,8 @@ L(V v){R sqrtf(v%v);}V Z(V v){R v*(1/L(v));}V M(V p){V q=p;q.z=0;F l=L(q),g=A(l-
 :.07;f=L({I(f,-1)-f,I(-p.z-4)+p.z+4})-1;g=I(-p.z,p.z+250);R{I(g,f),g<f?.8f:m};}
 int main(){V b=Z({-6,-1,14}),l=Z({b.z,0,-b.x})*.001,u(b.y*l.z-b.z*l.y,b.z*l.x-b.
 x*l.z,b.x*l.y-b.y*l.x),o,c,d;puts("P6 960 720 255");for(F x,y=720,a,n,i,j;y--;)
-for(x=960;x--;o=c+1,c=V(c.x/o.x,c.y/o.y,c.z/o.z)*255 P.x)P.y)P.z))for(c=n=0;n++<
-99;c=c+(d.z<-.8?V(9,6,3):d.y>.3?V(3,4,6):0)*a*E)for(o={74,12,-170},d=Z(b+l*(480-
-x+S())+u*(y-360+S())),a=1,i=4;--i;){V s=1,m;for(j=30;--j&&s.x>E;)s=M(o=o+d*s.x);
+for(x=960;x--;o=c+1,c=V(c.x/o.x,c.y/o.y,c.z/o.z)*255 P.x)P.y)P.z))for(c=n=0;a=n
+++<99;c=c+(d.z<-.8?V(9,6,3):d.y>.3?V(3,4,6):0)*a*E)for(o={74,12,-170},d=Z(b+l*(
+480-x+S())+u*(y-360+S())),i=4;--i;){V s=1,m;for(j=30;--j&&s.x>E;)s=M(o=o+d*s.x);
 if(o%o<3e4){a*=s.y;for(j=6;--j&&d%d>.5;)d={S(),S(),S()};m=Z(V(M(o+V(E,0)).x,M(o+
 V(0,E)).x,M(o+V(0,0,E)).x)+-s.x),d=Z(m%d<0?d*-1:d),o=o+m*E;}}}// Daniel Langner
